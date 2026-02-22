610fa65bd021d13d571fad48e8422e2d70dbe736
Sruthimeena Ramasamy <sruthimeena1918@gmail.com>
2026-02-22 12:28:37 +0530

    Add files via upload

---
+++ CodeAlpha_Portfolio/script3.js
@@ -0,0 +1,12 @@
+document.getElementById("gotohome").addEventListener("click", function() {
+    document.getElementById("home").scrollIntoView({behavious: "smooth"});
+});
+document.getElementById("gotop").addEventListener("click", function() {
+    document.getElementById("projects").scrollIntoView({behavious: "smooth"});
+});
+document.getElementById("gotoabout").addEventListener("click", function() {
+    document.getElementById("about").scrollIntoView({behavious: "smooth"});
+});
+document.getElementById("hire").addEventListener("click", function() {
+    document.getElementById("about").scrollIntoView({behavious: "smooth"});
+});
